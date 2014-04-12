85384870b236b6df53b851a9717742309df37394
voddan <dgvodopyan@gmail.com>
2014-04-12 17:21:27 +0400

    the second try with print_tab()

--- expr_tree.hpp
+++ expr_tree.hpp
@@ -14,9 +14,36 @@
 #include <string>
 #include <algorithm>
 #include <sstream>
+#include <iostream>
+
+#include "token.hpp"
 
 using namespace std;
 //----------------------------------------------------------------------
+
+// where should I place private helper classes like this?
+/*
+class Tab {
+	public:
+		Tab(int tab): _tab(tab) {}
+		friend ostream& operator<< (ostream& stream, const Tab& tab);
+	private:
+		const int _tab;
+};
+
+ostream& operator<< (ostream& stream, const Tab& tab) {
+	for (int i = tab._tab; i > 0; i--) {
+		stream << '\t';
+	}
+	return stream;
+} // */
+
+ostream& print_tab(ostream& stream, int tab){
+	for (int i = tab; i > 0; i--) {
+		stream << '\t';
+	}
+	return stream;
+}
 
 namespace expr_tree { // namespace expr_tree
 
@@ -27,29 +54,35 @@ class Expression{
 		const string name() const {return _name;}
 		
 		//---------------------------------
-		virtual string to_string() const{
+		virtual string to_string(int tab) const{
 			ostringstream str;
-			str << "Expression(" << _name << ")";
+			//str << Tab(tab) << "Expression(" << _name << ")\n";
+			print_tab(str, tab) << "Expression(" << _name << ")\n";
 			return str.str();
 		}
+		
+		virtual void set_link(Expression* link) = 0;
+		virtual Expression* get_link() const 	= 0;
 		
 	private:
 		const string _name;
 };
 
-class Expr_tree  { // not an expression
+class Expr_tree  { // not an Expression
 	public:
 		Expr_tree(Expression* expr) : _expr(expr) {}
 		~Expr_tree() { 
 			#ifdef DEBUG 
-				std::cout << "Expr_tree is out\n"; 
+				std::cerr << "Expr_tree is out\n"; 
 			#endif
 			delete _expr; 
 		}
 		
 		string to_string() const{
 			ostringstream str;
-			str << "Expr_tree(" << ((_expr)? _expr->to_string() : " null ") << ")";
+			str << "Expr_tree((" << '\n'
+			    << ((_expr)? _expr->to_string(1) : "\tnull\n") 
+			    << "))\n";
 			return str.str();
 		}
 		
@@ -66,18 +99,27 @@ class Expr_tree  { // not an expression
 class Node: public Expression {
 	public:
 		Node(Expression* link = 0) : Expression("Node"), _link(link) {}
-		~Node() { delete _link; }
+		virtual ~Node() { delete _link; }
 		
-		string to_string() const{
+		virtual string to_string(int tab) const{
 			ostringstream str;
-			str << "Node(" << ((_link)? _link->to_string() : " null ") << ")";
+			
+			//str << Tab(tab) << "Node(\n";
+			print_tab(str, tab) << "Node(\n";
+			
+			if (_link) { str << _link->to_string(tab + 1);}
+			else 	   { print_tab(str, tab + 1) << "null\n"; }
+			//else 	   { str << Tab(tab + 1) << "null\n"; }
+			
+			//str << Tab(tab) << ")\n";
+			print_tab(str, tab) << ")\n";
 			return str.str();
 		}
 		
-		void set_Link(Expression* link) { _link = link; }
-		Expression* get_Link() const 	{ return _link; }
+		virtual void set_link(Expression* link) { _link = link; }
+		virtual Expression* get_link() const 	{ return _link; }
 	private:
-		Expression* _link;
+		Expression* _link; // will children get this?
 };
 
 
@@ -86,45 +128,75 @@ class Num: public Expression {
 	public:
 		Num(int value) : Expression("Num"), _value(value) {}
 		
-		string to_string() const{
+		string to_string(int tab) const{
 			ostringstream str;
-			str << "Num(" << _value << ")";
+			//str << Tab(tab) << "Num(" << _value << ")\n";
+			print_tab(str, tab) << "Num(" << _value << ")\n";
 			return str.str();
+		}
+		
+		virtual void set_link(Expression* link) {
+			std::cerr << "Unsupported operation expr_tree::Num.set_link()\n";
+		}
+		virtual Expression* get_link() const {
+			std::cerr << "Unsupported operation expr_tree::Num.get_link()\n";
+			return 0;
 		}
 	private:
 		const int _value;
 };
 
+//*
 class B_Oper: public Expression{
 	public:
 		B_Oper(string name, Expression* left = 0, Expression* right = 0) : 
 			Expression(name), _left(left), _right(right) {} // & vs *
 		virtual ~B_Oper() {delete _left; delete _right;}
 		
-		string to_string() const{
+		string to_string(int tab) const{
 			ostringstream str;
-			str << "B-Oper(" << ((_left)? _left->to_string() : " null ")
-					 << "__" << name() << "__" 
-					 << ((_right)? _right->to_string() : " null ") << ")";
+			
+			print_tab(str, tab) << "B-Oper{" << '\n';
+			
+			if (_left) { str << _left->to_string(tab + 1);}
+			else 	   { print_tab(str, tab + 1) << "null\n"; }
+			//else 	   { str << Tab(tab + 1) << "null\n"; }
+			
+			print_tab(str, tab + 1) << "__" << name() << "__" << "\n";
+			
+			if (_right) { str << _right->to_string(tab + 1);}
+			else 	    { print_tab(str, tab + 1) << "null\n"; }
+			
+			print_tab(str, tab) << "}\n";
+			
 			return str.str();
 		}
 		
-		void set_Left(Expression* left) { _left = left; }
-		Expression* get_Left() const 	{ return _left; }
+		void set_left(Expression* left) { _left = left; }
+		Expression* get_left() const 	{ return _left; }
 		
-		void set_Right(Expression* right) { _right = right; }
-		Expression* get_Rightt() const 	  {  return _right; }
+		void set_right(Expression* right) { _right = right; }
+		Expression* get_rightt() const 	  {  return _right; }
+		
+		virtual void set_link(Expression* link) { _right = link; }
+		virtual Expression* get_link() const 	{ return _right; }
 		
 		//virtual int apply(int left, int right)
 	private:
 		Expression* _left;
 		Expression* _right;
-};
+}; // */
 
 class Plus : public B_Oper {
 	public: 
 		Plus(Expression* left = 0, Expression* right = 0) : B_Oper("+", left,  right) {}
 };
+
+} // namespace expr_tree
+////////////////////////////////////////////////////////////////////////
+namespace expr_tree {
+	
+Expr_tree& parse_to_Expr_tree(token::list_Token& lst);
 
 } // namespace expr_tree
 ////////////////////////////////////////////////////////////////////////
